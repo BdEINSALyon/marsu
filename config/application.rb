2972a67589986b18ea47a8cf8b3f49d75c669d6c
Philippe Vienne <philippegeek@gmail.com>
2016-08-25 22:58:34 +0200

    Added user

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Marsu
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    I18n.available_locales = [:fr]
+    I18n.available_locales = [:en, :fr]
     I18n.default_locale = :fr
 
   end
