b1032823599574a4058d9b72ac534332fc1cd184
Philippe Vienne <philippegeek@gmail.com>
2016-08-27 13:21:02 +0200

    Do not edit my own routes with a generator !!

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   resources :students do
     resources :memberships, only: [:index]
     resources :payments, only: [:create, :update, :index]
   end
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, ActiveAdmin::Devise.config, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'students#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
