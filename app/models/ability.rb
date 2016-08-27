6f1e9a98bdc4bd1ba315a8559e71f98ddea615b3
Philippe Vienne <philippegeek@gmail.com>
2016-08-27 13:05:47 +0200

    Added abilities and procfile

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,27 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
+    user ||= User.new
+
+    if user.email == 'philippe.vienne@bde-insa-lyon.fr'
+      can :manage, :all
+    end
+
+    if user.has_role? :admin
+      can :manage, :all
+    end
+
+    if user.has_role? :permanencier
+      can [:create, :update, :read], Student
+      can [:create, :read], Payment
+      can [:read], Membership
+    end
+
+    if user.has_role? :trezo
+      can [:update], Payment
+    end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
