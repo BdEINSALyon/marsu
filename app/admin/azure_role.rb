be81519ff295e2349a3980beb00c9f8d7dba5626
Philippe Vienne <philippegeek@gmail.com>
2016-08-28 13:31:08 +0200

    Login with access rights from office 365

--- app/admin/azure_role.rb
+++ app/admin/azure_role.rb
@@ -1,20 +1,28 @@
 require 'rest-client'
 ActiveAdmin.register AzureRole do
-  menu label: -> {t('admin.azure_role')}
+  menu label: -> {t('admin.azure_role.menu')}
+  permit_params :name, :azure_gid, :roles => []
 
   form do |f|
+    f.inputs 'MARSU Permissions' do
+      f.input :name
+      f.input :roles, collection: ['admin', 'permanencier', 'tresorier'], as: :check_boxes, multiple: true
+    end
     def fetch_azure_groups
       r = RestClient.get("https://graph.microsoft.com/v1.0/groups", :Authorization => "Bearer #{current_user.azure_token}")
       JSON.parse(r.body)['value'].pluck 'displayName', 'id'
     rescue => _
-      ['Unable to fetch groups from Azure, please logout and login again.']
+      []
     end
-    f.inputs 'Azure' do
-      f.input :azure_gid, collection: fetch_azure_groups
-    end
-    f.inputs 'MARSU Permissions' do
-      f.input :name
-      f.input :roles, collection: ['admin', 'permanencier', 'tresorier'], as: :check_boxes, multiple: true
+    azure_groups = fetch_azure_groups
+    if azure_groups.length > 0
+      f.inputs 'Azure' do
+        f.input :azure_gid, collection: azure_groups
+      end
+    else
+      f.inputs 'Azure (Error)' do
+        f.input :azure_gid, placeholder: t('admin.azure_role.could_not_fetch_enter_gid')
+      end
     end
     f.actions
   end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,9 +6,44 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       user.password_confirmation = pass
     end
 
+    # Clear permissions for a user who has not been logged since a long
+    if @user.updated_at < (Rails.env.development? ? 2.minutes : 1.month)
+      @user.roles = []
+      @user.save
+    end
+
+    # Save access token
     @user.update azure_token: request.env["omniauth.auth"].credentials.token
 
+    # Check if user is tenant admin
+    begin
+      r = RestClient.get("https://graph.microsoft.com/v1.0/me/memberOf", :Authorization => "Bearer #{current_user.azure_token}")
+      admin_groups = JSON.parse(r.body)['value'].select {|g| g['@odata.type']== '#microsoft.graph.directoryRole'}
+      if admin_groups.length > 0 and admin_groups[0]['displayName'] == 'Company Administrator'
+        @user.add_role 'admin'
+      end
+    rescue
+      # ignored
+    end
 
+    # Retreive local known groups for permissions
+    azure = AzureRole.all.pluck :azure_gid
+    user_azure_groups = []
+
+    # Ask to Azure if the user is in one of this groups
+    azure.each_slice(20).to_a.each do |groups_to_check|
+      begin
+        r = RestClient.post "https://graph.microsoft.com/v1.0/me/checkMemberGroups",{groupIds:groups_to_check}.to_json, content_type: :json, :Authorization => "Bearer #{@user.azure_token}"
+        # Store the response
+        user_azure_groups = user_azure_groups + JSON.parse(r.body)['value']
+      rescue => _
+      end
+    end
+
+    # For each valid group found, add the permissions to the user
+    AzureRole.where(azure_gid: user_azure_groups).each do |azure_role|
+      azure_role.roles.each {|role| @user.add_role role unless role.empty?}
+    end
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
