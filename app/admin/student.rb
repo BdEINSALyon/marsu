2c037c1547e665d454d54b3af6345d002aedef2d
Philippe Vienne <philippegeek@gmail.com>
2016-09-09 17:33:54 +0200

    Add payments to students

--- app/admin/student.rb
+++ app/admin/student.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Student do
   menu priority: 2, id: 'Student'
+  sidebar I18n.t('admin.student.payments'), only: :show do
+    student.payments.each do |p|
+      attributes_table_for p do
+        row(:payable) { |b| link_to b.payable.name, admin_payment_path(b) }
+        row(I18n.t('membership.refunded')) { |b| status_tag b.refunded }
+      end
+    end
+  end
 end
