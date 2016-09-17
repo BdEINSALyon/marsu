bf396dcc81b29cb776a1c44d9e6f053ea24d7a4f
Philippe Vienne <philippegeek@gmail.com>
2016-09-17 12:04:26 +0200

    Validated Mail by @crocmagnon

--- app/mailers/va_mailer.rb
+++ app/mailers/va_mailer.rb
@@ -7,6 +7,6 @@ class VaMailer < ApplicationMailer
   #
   def va(student)
     @student = student
-    mail to: @student.email, from: 'BdE INSA Lyon <orgaif@mail.adhesion.bde-insa-lyon.fr>', reply_to: 'contact@bde-insa-lyon.fr', subject: "#{@student.first_name}, (re)découvre ta carte VA ..."
+    mail to: @student.email, from: 'BdE INSA Lyon <orgaif@mail.adhesion.bde-insa-lyon.fr>', reply_to: 'contact@bde-insa-lyon.fr', subject: "#{@student.first_name}, (re)découvre ta carte VA…"
   end
 end
