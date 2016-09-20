33d9e74975cca87990bc88d8e829ec018f1e5faa
Philippe Vienne <philippegeek@gmail.com>
2016-09-20 10:49:32 +0200

    Small correction for CORS

--- app/controllers/api/version1/api_controller.rb
+++ app/controllers/api/version1/api_controller.rb
@@ -1,12 +1,12 @@
 class Api::Version1::ApiController < ApplicationController
   rescue_from ActionController::ParameterMissing, with: :parameter_missing
+  before_action :cors_check
+  before_action :cors_set
   before_action :check_api_access
 
   # noinspection RailsParamDefResolve
   skip_before_filter :verify_authenticity_token
 
-  before_action :cors_check
-  after_action :cors_set
 
   def cors
     head(:ok) if request.request_method == 'OPTIONS'
@@ -58,7 +58,7 @@ class Api::Version1::ApiController < ApplicationController
     if request.method == 'OPTIONS'
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
       headers['Access-Control-Max-Age'] = '1728000'
 
       render :text => '', :content_type => 'text/plain'
