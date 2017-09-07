0a64e2675990970809ca5269f0d3746fe9b9fed0
Philippe Vienne <philippegeek@gmail.com>
2017-09-07 02:13:00 +0200

    Register logs of any request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   include HttpAcceptLanguage::AutoLocale
   protect_from_forgery with: :exception
+  after_filter :log_action
+
+  def log_action
+    if current_user
+      LogEntry.create(
+        user: current_user,
+        ip: request.ip,
+        params: params.as_json,
+        controller: controller_name,
+        action: action_name,
+        path: request.path,
+        method: request.method,
+        status: response.status
+      )
+    end
+  end
 end
