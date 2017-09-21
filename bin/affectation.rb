912ea4d5e0f537d764a97c32e4fe5d3a841a247d
Philippe Vienne <philippe@vienne.me>
2017-09-21 18:32:26 +0200

    Add affectation script

---
+++ bin/affectation.rb
@@ -0,0 +1,159 @@
+WeiBungalow.destroy_all
+
+# Créer les bungalow
+WeiBungalow.create(name: "B09", wei_bus_id: 18, seats: 4)
+WeiBungalow.create(name: "B10", wei_bus_id: 18, seats: 4)
+WeiBungalow.create(name: "B11", wei_bus_id: 18, seats: 4)
+WeiBungalow.create(name: "B13", wei_bus_id: 18, seats: 4)
+WeiBungalow.create(name: "B14", wei_bus_id: 18, seats: 4)
+WeiBungalow.create(name: "B15", wei_bus_id: 18, seats: 4)
+WeiBungalow.create(name: "C01", wei_bus_id: 10, seats: 6)
+WeiBungalow.create(name: "C02", wei_bus_id: 10, seats: 6)
+WeiBungalow.create(name: "C03", wei_bus_id: 10, seats: 6)
+WeiBungalow.create(name: "C04", wei_bus_id: 10, seats: 6)
+WeiBungalow.create(name: "C05", wei_bus_id: 10, seats: 6)
+WeiBungalow.create(name: "C06", wei_bus_id: 10, seats: 4)
+WeiBungalow.create(name: "C07", wei_bus_id: 10, seats: 6)
+WeiBungalow.create(name: "C08", wei_bus_id: 10, seats: 6)
+WeiBungalow.create(name: "C09", wei_bus_id: 10, seats: 6)
+WeiBungalow.create(name: "C10", wei_bus_id: 11, seats: 6)
+WeiBungalow.create(name: "C11", wei_bus_id: 11, seats: 6)
+WeiBungalow.create(name: "C12", wei_bus_id: 11, seats: 6)
+WeiBungalow.create(name: "IR03", wei_bus_id: 17, seats: 6)
+WeiBungalow.create(name: "IR04", wei_bus_id: 17, seats: 6)
+WeiBungalow.create(name: "IR05", wei_bus_id: 17, seats: 6)
+WeiBungalow.create(name: "IR06", wei_bus_id: 17, seats: 6)
+WeiBungalow.create(name: "IR07", wei_bus_id: 15, seats: 6)
+WeiBungalow.create(name: "IR08", wei_bus_id: 15, seats: 6)
+WeiBungalow.create(name: "IR09", wei_bus_id: 15, seats: 6)
+WeiBungalow.create(name: "IR10", wei_bus_id: 12, seats: 6)
+WeiBungalow.create(name: "IR12", wei_bus_id: 12, seats: 6)
+WeiBungalow.create(name: "IR13", wei_bus_id: 12, seats: 6)
+WeiBungalow.create(name: "IR14", wei_bus_id: 12, seats: 6)
+WeiBungalow.create(name: "IR15", wei_bus_id: 12, seats: 6)
+WeiBungalow.create(name: "IR16", wei_bus_id: 11, seats: 6)
+WeiBungalow.create(name: "IR17", wei_bus_id: 11, seats: 6)
+WeiBungalow.create(name: "IR18", wei_bus_id: 11, seats: 6)
+WeiBungalow.create(name: "IR19", wei_bus_id: 11, seats: 6)
+WeiBungalow.create(name: "IR20", wei_bus_id: 11, seats: 6)
+WeiBungalow.create(name: "IR21", wei_bus_id: 11, seats: 6)
+WeiBungalow.create(name: "IR22", wei_bus_id: 12, seats: 6)
+WeiBungalow.create(name: "IR23", wei_bus_id: 12, seats: 6)
+WeiBungalow.create(name: "IR24", wei_bus_id: 12, seats: 6)
+WeiBungalow.create(name: "M03", wei_bus_id: 18, seats: 6)
+WeiBungalow.create(name: "M04", wei_bus_id: 18, seats: 6)
+WeiBungalow.create(name: "M05", wei_bus_id: 18, seats: 4)
+WeiBungalow.create(name: "M06", wei_bus_id: 18, seats: 4)
+WeiBungalow.create(name: "M07", wei_bus_id: 18, seats: 4)
+WeiBungalow.create(name: "M17", wei_bus_id: 13, seats: 6)
+WeiBungalow.create(name: "M18", wei_bus_id: 13, seats: 6)
+WeiBungalow.create(name: "M19", wei_bus_id: 13, seats: 6)
+WeiBungalow.create(name: "M20", wei_bus_id: 13, seats: 6)
+WeiBungalow.create(name: "M21", wei_bus_id: 13, seats: 6)
+WeiBungalow.create(name: "M22", wei_bus_id: 13, seats: 6)
+WeiBungalow.create(name: "M23", wei_bus_id: 13, seats: 6)
+WeiBungalow.create(name: "M24", wei_bus_id: 13, seats: 6)
+WeiBungalow.create(name: "SF02", wei_bus_id: 17, seats: 6)
+WeiBungalow.create(name: "SF03", wei_bus_id: 17, seats: 6)
+WeiBungalow.create(name: "SF04", wei_bus_id: 17, seats: 6)
+WeiBungalow.create(name: "SF05", wei_bus_id: 17, seats: 6)
+WeiBungalow.create(name: "SF06", wei_bus_id: 16, seats: 6)
+WeiBungalow.create(name: "SF07", wei_bus_id: 16, seats: 4)
+WeiBungalow.create(name: "SF08", wei_bus_id: 16, seats: 4)
+WeiBungalow.create(name: "SF09", wei_bus_id: 16, seats: 4)
+WeiBungalow.create(name: "SF10", wei_bus_id: 16, seats: 4)
+WeiBungalow.create(name: "SF11", wei_bus_id: 16, seats: 4)
+WeiBungalow.create(name: "SF12", wei_bus_id: 16, seats: 4)
+WeiBungalow.create(name: "SF13", wei_bus_id: 16, seats: 4)
+WeiBungalow.create(name: "SF14", wei_bus_id: 16, seats: 4)
+WeiBungalow.create(name: "SF15", wei_bus_id: 16, seats: 4)
+WeiBungalow.create(name: "SF16", wei_bus_id: 16, seats: 4)
+WeiBungalow.create(name: "SF17", wei_bus_id: 15, seats: 4)
+WeiBungalow.create(name: "SF18", wei_bus_id: 15, seats: 4)
+WeiBungalow.create(name: "SF19", wei_bus_id: 15, seats: 4)
+WeiBungalow.create(name: "SF20", wei_bus_id: 15, seats: 4)
+WeiBungalow.create(name: "SF21", wei_bus_id: 15, seats: 4)
+WeiBungalow.create(name: "SF24", wei_bus_id: 15, seats: 6)
+WeiBungalow.create(name: "SF25", wei_bus_id: 15, seats: 6)
+WeiBungalow.create(name: "SF26", wei_bus_id: 14, seats: 6)
+WeiBungalow.create(name: "SF27", wei_bus_id: 14, seats: 6)
+WeiBungalow.create(name: "SF28", wei_bus_id: 14, seats: 6)
+WeiBungalow.create(name: "SF29", wei_bus_id: 14, seats: 6)
+WeiBungalow.create(name: "SF30", wei_bus_id: 14, seats: 6)
+WeiBungalow.create(name: "SF31", wei_bus_id: 14, seats: 6)
+WeiBungalow.create(name: "SF32", wei_bus_id: 14, seats: 6)
+WeiBungalow.create(name: "SF34", wei_bus_id: 14, seats: 6)
+WeiBungalow.create(name: "IR01", seats: 6)
+WeiBungalow.create(name: "IR02", seats: 6)
+WeiBungalow.create(name: "SF01", seats: 6)
+
+
+biz = WeiRegistration.registered
+
+biz_m = biz.joins(:student).where(students: {gender: 'M'})
+biz_w = biz.joins(:student).where(students: {gender: 'W'})
+
+# Première passe d'affectation homme et femme
+
+biz_m.where(wei_bungalow_id: nil).each do |b|
+  bung = WeiBungalow.not_full.where(wei_bus: b.wei_bus).first
+  if bung
+    if bung.wei_registrations_count <= 0
+      # Ne pas créer de bungalow partiellement remplis
+      if biz_m.where(wei_bus: b.wei_bus, wei_bungalow: nil).count < bung.seats
+        next
+      end
+    end
+    bung.update gender: 'M'
+    b.update wei_bungalow: bung
+  end
+end
+
+biz_w.where(wei_bungalow_id: nil).each do |b|
+  bung = WeiBungalow.not_full.where(wei_bus: b.wei_bus).first
+  if bung
+    if bung.wei_registrations_count <= 0
+      if biz_w.where(wei_bus: b.wei_bus, wei_bungalow: nil).count < bung.seats
+        next
+      end
+    end
+    bung.update gender: 'W'
+    b.update wei_bungalow: bung
+  end
+end
+
+biz_m.where(wei_bungalow_id: nil).each do |b|
+  bung = WeiBungalow.not_full.first
+  if bung
+    bung.update gender: 'M'
+    b.update wei_bungalow: bung
+  end
+end
+
+last_bung = WeiBungalow.not_full.where(wei_registrations_count: 0)
+
+biz_w.where(wei_bungalow_id: nil).each do |b|
+  bung = last_bung.not_full.first
+  if bung
+    bung.update gender: 'W'
+    b.update wei_bungalow: bung
+  end
+end
+
+biz_w.where(wei_bungalow_id: nil).each do |b|
+  bung = WeiBungalow.not_full.where(gender: 'W').first
+  if bung
+    b.update wei_bungalow: bung
+  end
+end
+
+WeiBungalow.not_full.update(gender: nil)
+
+biz.where(wei_bungalow_id: nil).each do |b|
+  bung = WeiBungalow.not_full.first
+  if bung
+    b.update wei_bungalow: bung
+  end
+end
+
+biz.where(wei_bungalow_id: nil).count
